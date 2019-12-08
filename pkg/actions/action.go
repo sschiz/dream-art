3af1a6c1f2bb99a67b75bcce9d466770f797adf9
Alexander <sav3nme@gmail.com>
2019-12-08 15:12:09 +0300

    actions: add Next method in action interface

--- pkg/actions/action.go
+++ pkg/actions/action.go
@@ -6,11 +6,12 @@ import "github.com/sschiz/dream-art/pkg/shop"
 
 // Action is abstract which provides action handling
 type Action interface {
-	Execute() error                                     // do what you need
-	IsDone() bool                                       // check if action is done
-	SetDone()                                           // set action that is done, but if all chunks is collected
-	AddChunk(chunk interface{}, isLastChunk bool) error // add chunk that is needed
-	IsChunksCollected() bool                            // returns all chunks collected
+	Execute() error                   // do what you need
+	IsDone() bool                     // check if action is done
+	SetDone()                         // set action that is done, but if all chunks is collected
+	AddChunk(chunk interface{}) error // add chunk that is needed
+	IsChunksCollected() bool          // returns all chunks collected
+	Next() string                     // return text for next chunk
 }
 
 var (
