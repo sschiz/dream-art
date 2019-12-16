d893f7dcaefa812c1e3e5bf8094abfcfde539b15
Alexander <sav3nme@gmail.com>
2019-12-16 14:18:09 +0300

    shop: add DeleteAdmin method

--- pkg/shop/shop.go
+++ pkg/shop/shop.go
@@ -17,6 +17,7 @@ type Shop struct {
 var (
 	ErrAdminAlreadyExists = errors.New("such an admin already exists")
 	ErrWrongNickname      = errors.New("wrong nickname")
+	ErrAdminDoesNotExist  = errors.New("admin does not exist")
 )
 
 // AppendAdmin appends new admin into slice by admin's nickname.
@@ -35,6 +36,25 @@ func (s *Shop) AppendAdmin(name string) error {
 	} else {
 		s.admins = append(s.admins[:i], append([]string{name}, s.admins[i:]...)...)
 	}
+
+	return nil
+}
+
+// DeleteAdmin removes admin nickname from list of admins
+func (s *Shop) DeleteAdmin(name string) error {
+	if !strings.HasPrefix(name, "@") {
+		return ErrWrongNickname
+	}
+
+	name = name[1:]
+
+	i := sort.SearchStrings(s.admins, name)
+
+	if !(i < len(s.admins) && s.admins[i] == name) {
+		return ErrAdminDoesNotExist
+	}
+
+	s.admins = append(s.admins[:i], s.admins[i+1:]...)
 
 	return nil
 }
