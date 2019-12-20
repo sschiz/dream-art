619173a847a643fa2a0079bb8e0b543d60f74589
Alexander <sav3nme@gmail.com>
2019-12-20 22:48:25 +0300

    product: delete limitations for name and desc

--- pkg/product/product.go
+++ pkg/product/product.go
@@ -5,15 +5,7 @@
 
 package product
 
-import "regexp"
-
 import "errors"
-
-// ErrWrongTitle is error which provides error for titles
-var ErrWrongTitle = errors.New("title is wrong")
-
-// ErrWrongText is error which provides error for texts
-var ErrWrongText = errors.New("text is wrong")
 
 // ErrWrongText is error which provides error for ids
 var ErrWrongId = errors.New("ID is wrong")
@@ -29,18 +21,6 @@ type Product struct {
 
 // NewProduct creates product checking the name and description
 func NewProduct(name, description, photo string, price uint, id int) (*Product, error) {
-	matched, _ := regexp.MatchString(`^([a-zа-я0-9 _.])+$`, name)
-
-	if !matched {
-		return nil, ErrWrongTitle
-	}
-
-	matched, _ = regexp.MatchString(`^([\w\sА-Яа-я0-9.,!?:;])+$`, description)
-
-	if !matched {
-		return nil, ErrWrongText
-	}
-
 	if id < 0 {
 		return nil, ErrWrongId
 	}
